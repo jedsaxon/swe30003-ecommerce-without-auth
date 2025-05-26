e26265b79ff6e155e8de13d2e9fa6360d1828e60
jedsaxon <jed-sax@proton.me>
2025-05-27 08:38:19 +1000

    Add data access class with sqlite connection

--- DataAccess/DataAccess.cs
+++ DataAccess/DataAccess.cs
@@ -1,6 +1,24 @@
+using Microsoft.Data.Sqlite;
+
 namespace DataAccess;
 
-public class DataAccess
+public class DataAccess : IAsyncDisposable, IDisposable
 {
-    
+    private readonly SqliteConnection _connection;
+
+    public DataAccess(string connectionString)
+    {
+        _connection = new SqliteConnection(connectionString);
+    }
+
+
+    public void Dispose()
+    {
+        _connection.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _connection.DisposeAsync();
+    }
 }
