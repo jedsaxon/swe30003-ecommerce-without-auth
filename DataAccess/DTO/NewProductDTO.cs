51bffd7dc805a2df4800fc5f8d1f95a73be089a4
jedsaxon <jed-sax@proton.me>
2025-05-27 12:47:27 +1000

    Add product seeder

--- DataAccess/DTO/NewProductDTO.cs
+++ DataAccess/DTO/NewProductDTO.cs
@@ -1,3 +1,18 @@
 namespace DataAccess.DTO;
 
-public record NewProductDTO(string Name, string ShortDescription, string LongDescription, double Price);
+public class NewProductDTO(
+    string name,
+    string shortDescription,
+    string longDescription,
+    double price)
+{
+    public string Name { get; set; } = name;
+    public string ShortDescription { get; set; } = shortDescription;
+    public string LongDescription { get; set; } = longDescription;
+    public double Price { get; set; } = price;
+
+    public NewProductDTO() : this(string.Empty, string.Empty, string.Empty, 0)
+    {
+
+    }
+}
