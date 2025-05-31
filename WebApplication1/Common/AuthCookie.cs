58c59d6ea2a7dcd5f854e4fee91f2888d1215566
Jed Saxon <jed-saxon@pm.me>
2025-05-31 12:34:26 +1000

    Create AuthCookie class for storing user auth state

---
+++ WebApplication1/Common/AuthCookie.cs
@@ -0,0 +1,19 @@
+namespace WebApplication1.Common;
+
+/// <summary>
+/// When a user logs in, they need to store their details in a cookie so the site can display it correctly. This cookie is
+/// therefore created to store user details and be serializable by json
+/// </summary>
+public class AuthCookie
+{
+    public Guid UserId { get; init; }
+
+    public string FirstName { get; init; } = string.Empty;
+        
+
+    public string LastName { get; init; } = string.Empty;
+
+    public string EmailAddress { get; init; } = string.Empty;
+
+    public string PhoneNumber { get; init; } = string.Empty;
+}
