fea4f9dd4d5713ef178f3bea875e11bc50411cd5
Alex <godrak118@gmail.com>
2025-05-31 18:20:25 +1000

    finish cart controller

---
+++ WebApplication1/ViewModel/ViewShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.ViewModel;
+
+public class ViewShoppingCartViewModel
+{
+    [Required] public IEnumerable<CartItemViewModel> Items = new HashSet<CartItemViewModel>();
+}
+
+public class CartItemViewModel
+{
+    [Required] [HiddenInput] public Guid ProductId { get; init; }
+    [Required] [MaxLength(256)] public string ProductName { get; init; } = "";
+    [Required] [Range(1, int.MaxValue)] public int Count { get; init; } = 1;
+}
