a8b435a5623e20c1878d7f561df78f3c26ec886d
Jed Saxon <jed-saxon@pm.me>
2025-05-31 11:09:07 +1000

    Add create account page with validation

--- WebApplication1/ViewModel/CreateAccountViewModel.cs
+++ WebApplication1/ViewModel/CreateAccountViewModel.cs
@@ -12,5 +12,5 @@ public class CreateAccountViewModel
 
     [Required, Phone] public string PhoneNumber { get; set; } = null!;
 
-    [Required] public string Address { get; } = null!;
+    [Required] public string Password { get; set; } = null!;
 }
